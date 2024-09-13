a94df28ae01181138b416a6d918ceb8212f53b4e
Christian N-G <cnatali0@email.cpcc.edu>
2024-09-13 15:27:51 -0400

    Project_Christian_NataliGergich

---
+++ Project_Christian_NataliGergich.java
@@ -0,0 +1,11 @@
+import java.util. *; // package, collection of related classes
+
+public class Project_Christian_NataliGergich
+{
+   public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+    
+      System.out.print("!");  
+   }
+}      
