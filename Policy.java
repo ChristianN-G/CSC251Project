2d6afb1e549c42c9d7c245e0450b6ffeb8bf35b1
Christian N-G <cnatali0@email.cpcc.edu>
2024-11-10 18:34:27 -0500

    Update Policy.java
    Moved fields and methods to the PolicyHolder class

--- Policy.java
+++ Policy.java
@@ -1,74 +1,31 @@
 public class Policy // File name
 {
-   // instance/data field to hold the policy number, provider name, first and last name, policy holder age, smoking status, policy holder height, and weight of an insurance policy
+   // instance/data field to hold the policy number, provider name, and the policy holder information of an insurance policy
    private int policyNum;
    private String providerName;
-   private String firstName;
-   private String lastName;
-   private int pHolderAge;
-   private String smokeStatus;
-   private double pHolderHeight;
-   private double pHolderWeight;
+   private PolicyHolder policyHolder;
    
    /**
-      no-arg constructor that initializes the policy number, provider name, first and last name, policy holder age, smoking status, policy holder height, and weight of an insurance policy to default values
+      no-arg constructor that initializes the policy number, provider name, and the policy holder's information of an insurance policy to default values
    */
    public Policy()
    {
       policyNum = 0;
       providerName = "None";
-      firstName = "None";
-      lastName = "None";
-      pHolderAge = 0;
-      smokeStatus = "None";
-      pHolderHeight = 0.0;
-      pHolderWeight = 0.0;
+      policyHolder = null;
    }
    
    /**
-      Constructor that initializes the policy number, provider name, first and last name, policy holder age, smoking status, policy holder height, and weight of an insurance policy to the valaue of the argument it is
+      onstructor that initializes the policy number, provider name, and the policy holder's information of an insurance policy to the value of the argument passed
       @param policyNum The policy number.
       @param providerName The provider name.
-      @param firstName The policy holder's first name.
-      @param lastName The policy holder's last name.
-      @param pHolderAge The policy holder's age.
-      @param smokeStatus The policy holder's smoking status.
-      @param pHolderHeight The policy holder's height.
-      @param pHolderWeight The policy holder's weight.
+      @param policyHolder The policy holder's information.
    */
-   public Policy(int num, String pName, String fName, String lName, int age, String sStatus, double height, double weight)
+   public Policy(int policyNum, String providerName, PolicyHolder policyHolder)
    {
-      policyNum = num;
-      providerName = pName;
-      firstName = fName;
-      lastName = lName;
-      pHolderAge = age;
-      smokeStatus = sStatus;
-      pHolderHeight = height;
-      pHolderWeight = weight;
-   }
-
-   /**
-      mutator (setter) method
-      @param policyNum The policy number.
-      @param providerName The provider name.
-      @param firstName The policy holder's first name.
-      @param lastName The policy holder's last name.
-      @param pHolderAge The policy holder's age.
-      @param smokeStatus The policy holder's smoking status.
-      @param pHolderHeight The policy holder's height.
-      @param pHolderWeight The policy holder's weight.
-   */
-   public void setPolicy(int num, String pName, String fName, String lName, int age, String sStatus, double height, double weight)
-   {
-      policyNum = num;
-      providerName = pName;
-      firstName = fName;
-      lastName = lName;
-      pHolderAge = age;
-      smokeStatus = sStatus;
-      pHolderHeight = height;
-      pHolderWeight = weight;
+      this.policyNum = policyNum;
+      this.providerName = providerName;
+      this.policyHolder = policyHolder;
    }
    
    /**
@@ -91,67 +48,12 @@ public class Policy // File name
    
    /**
       Accessor (getter) method
-      @return The policy holder's first name.
+      @return The policy holder's information.
    */
-   public String getFirstName()
+   public PolicyHolder getPolicyHolder()
    {
-      return firstName;
-   }
-   
-   /**
-      Accessor (getter) method
-      @return The policy holder's last name.
-   */
-   public String getLastName()
-   {
-      return lastName;
-   }
-   
-   /**
-      Accessor (getter) method
-      @return The policy holder's age.
-   */
-   public int getPHolderAge()
-   {
-      return pHolderAge;
-   }
-   
-   /**
-      Accessor (getter) method
-      @return The policy holder's smoking status.
-   */
-   public String getSmokeStatus()
-   {
-      return smokeStatus;
-   }
-   
-   /**
-      Accessor (getter) method
-      @return The policy holder's height.
-   */
-   public double getPHolderHeight()
-   {
-      return pHolderHeight;
-   }
-   
-   /**
-      Accessor (getter) method
-      @return The policy holder's weight.
-   */
-   public double getPHolderWeight()
-   {
-      return pHolderWeight;
-   }
-   
-   /**
-      Accessor (getter) method
-      @return The policy holder's BMI.
-   */
-   public double getBMI()
-   {
-      double BMI = (pHolderWeight * 703) / (pHolderHeight * pHolderHeight);
-      return BMI;
-   }
+      return policyHolder;
+   } 
    
    /**
       Accessor (getter) method
@@ -159,33 +61,61 @@ public class Policy // File name
    */
    public double getFee(double BMI)
    {
-      if(pHolderAge > 50){
-         if(smokeStatus.equalsIgnoreCase("smoker") && BMI > 35){
-            return 600 + 75 + 100 + ((BMI-35) * 20);
+      if(policyHolder.getAge() > 50){
+         if(policyHolder.getSmokeStatus().equalsIgnoreCase("smoker") && policyHolder.getBMI() > 35){
+            return 600 + 75 + 100 + ((policyHolder.getBMI()-35) * 20);
          }
-         else if(smokeStatus.equalsIgnoreCase("smoker")){
+         else if(policyHolder.getSmokeStatus().equalsIgnoreCase("smoker")){
             return 600 + 75 + 100;
          }
-         else if(BMI > 35){
-            return 600 + 75 + ((BMI-35) * 20);
+         else if(policyHolder.getBMI() > 35){
+            return 600 + 75 + ((policyHolder.getBMI()-35) * 20);
          }
          else{
             return 600 + 75;
          }
       }
-      else if(smokeStatus.equalsIgnoreCase("smoker")){
-         if(smokeStatus.equalsIgnoreCase("smoker") && BMI > 35){
-            return 600 + 100 + ((BMI-35) * 20);
+      else if(policyHolder.getSmokeStatus().equalsIgnoreCase("smoker")){
+         if(policyHolder.getSmokeStatus().equalsIgnoreCase("smoker") && policyHolder.getBMI() > 35){
+            return 600 + 100 + ((policyHolder.getBMI()-35) * 20);
          }
          else{
             return 600 + 100;
          }
       }
-      else if(BMI > 35){
-         return 600 + ((BMI-35) * 20);
+      else if(policyHolder.getBMI() > 35){
+         return 600 + ((policyHolder.getBMI()-35) * 20);
       }
       else{
          return 600;
       }
    }
+   
+   /**
+      mutator (setter) method
+      @param policyNum The policy number.
+   */
+   public void setPolicyNum(int policyNum)
+   {
+      this.policyNum = policyNum;
+   }
+   
+   /**
+      mutator (setter) method
+      @param providerName The provider name.
+   */
+   public void setProviderName(String providerName)
+   {
+      this.providerName = providerName;
+   }
+   
+   /**
+      mutator (setter) method
+      @param policyHolder The policy holder's information..
+   */
+   public void setPolicyHolder(PolicyHolder policyHolder)
+   {
+      this.policyHolder = policyHolder;
+   }
+   
 }
